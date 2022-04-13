5f5b8fff0141723f8ee80ae0712245d87d77a906
tarik <63872091+tariksitki@users.noreply.github.com>
2022-04-13 19:47:54 +0200

    Navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 
-import React from 'react'
+import { useState } from "react";
+import { Hamburger, Logo, Menu, MenuLink, Nav } from "./NavbarStyles";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <Nav>
+      <Logo to={"/"} >RECIPE APP</Logo>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+          {/* icinde bilgi olmayacaksa spanlari empty element olarak tercih edelim */}
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+
+      <Menu isOpen = {isOpen}>
+        <MenuLink to={'/about'} >About</MenuLink>
+        <MenuLink to={'/github'} >Github</MenuLink>
+        <MenuLink to={'/logout'} >Logout</MenuLink>
+      </Menu>
+    </Nav>
   )
 }
 
